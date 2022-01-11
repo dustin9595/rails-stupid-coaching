47bafe8252aba81d665c1d9bd1922bca9a2e637b
Dustin Dillenburg <dustin.dillenburg@gmail.com>
2022-01-11 21:01:42 +0100

    stupid coaching rails app first push

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @search = params[:question]
+    if @search.downcase == 'i am going to work'
+      @answer = 'Great!'
+    elsif @search.end_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = 'I do not care, get dressed and go to work!'
+    end
+  end
+end
